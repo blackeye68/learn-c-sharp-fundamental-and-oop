b0a74d0d581e8612d7216ce241cf545be02c6790
tu.cuong.bat.tuc@gmail.com <24867193@vohoi>
2019-03-20 23:45:40 +0700

    sàđsgưèdsedfs

--- Complete_C_Sharp_Programming_Language/Practice_08/Program.cs
+++ Complete_C_Sharp_Programming_Language/Practice_08/Program.cs
@@ -10,12 +10,51 @@ namespace Practice_08
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.OutputEncoding = Encoding.UTF8;
+            //Write a C# Sharp program to print on screen the 
+            //output of adding, subtracting, multiplying and dividing of 
+            //two numbers which will be entered by the user.
+            decimal a, b;
+            char operation;
+            Console.WriteLine("Input a:");
+            a = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Input b:");
+            b = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Input operation (+, -, *, /, %):");
+            operation = Console.ReadLine()[0];
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine($"Adding of {a} + {b} = {a + b}");
+                    break;
+                case '-':
+                    Console.WriteLine($"Subtracting of {a} - {b} = {a - b}");
+                    break;
+                case '*':
+                    Console.WriteLine($"Multiplying of {a} * {b} = {a * b}");
+                    break;
+                case '/':
+                    if(a == 0 || b == 0)
+                    {
+                        Console.WriteLine("Error ! Không thể chia cho 0 hoặc 0 chia cho 1 số !");
+                    } else
+                    {
+                        Console.WriteLine($"Dividing of {a} / {b} = {a / b}");
+                    }
+                    break;
+                case '%':
+                    Console.WriteLine($"Modulo of {a} % {b} = {a % b}");
+                    break;
+                default:
+                    Console.WriteLine("Error ! Bạn đã không nhập vào một phép toán !");
+                    break;
+            }
+
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Complete_C_Sharp_Programming_Language/Practice_09/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //Write a C# Sharp program that takes a number as input and print its multiplication table.
+            //Test Data:
+            //Enter the number: 5
+            //Expected Output:
+            //5 * 0 = 0
+            //5 * 1 = 5
+            //5 * 2 = 10
+            //5 * 3 = 15
+            //....
+            //5 * 10 = 50
+
+            Console.ReadLine();
+        }
+    }
+}
